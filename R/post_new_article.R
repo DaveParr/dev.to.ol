bdde61b2043bb7a22739f0d958e282b98631974e
David Parr <davidrgparr@gmail.com>
2020-05-19 15:29:33 +0100

    document and tweak

--- R/post_new_article.R
+++ R/post_new_article.R
@@ -1,8 +1,24 @@
-post_new_article <- function(key = NA, article) {
-  body_string <-
-    '{"article": {"title": "Hello, World!","published": true,"body_markdown": "Hello DEV, this is my first post","tags": ["discuss","help"],"series": "Hello series","canonical_url": "https://example.com/blog/hello"}}'
+#' @title Post a markdown file to dev.to
+#' @description Create a new post well rendered markdown file
+#' @param key Your API key, Default: NA
+#' @param file The path to the file, Default: file
+#' @return The response
+#' @details Will look for an api key in the `.REnviron` file. Seems to check if the body is identical to a previous article and error if so with `"Body markdown has already been taken"`.
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  post_new_article("./articles/my_article.md")
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[httr]{POST}},\code{\link[httr]{add_headers}},\code{\link[httr]{verbose}},\code{\link[httr]{content}}
+#'  \code{\link[readr]{read_file}}
+#' @rdname post_new_article
+#' @export
+#' @importFrom httr POST add_headers verbose content
+#' @importFrom readr read_file
 
-  body_json <- jsonlite::fromJSON(body_string)
+post_new_article <- function(key = NA, file = file) {
 
   response <- httr::POST(
     url = "https://dev.to/api/articles",
@@ -13,8 +29,10 @@ post_new_article <- function(key = NA, article) {
                           message("Using DEVTO in .Reinviron")
                           Sys.getenv(x = "DEVTO")
                         }),
-    body = list(article = list(title = 'title',
-                               body_markdown= "Body")),
+    body = list(article = list(
+      title = 'title',
+      body_markdown = readr::read_file(file = file)
+    )),
     encode = 'json',
     httr::verbose()
   )
