8f15895061e639b01b2d498a8f3cc2e8c63d8fc6
David Parr <davidrgparr@gmail.com>
2020-06-01 18:07:59 +0100

    saner naming

--- tests/testthat/test-get_my_user.R
+++ tests/testthat/test-get_my_user.R
@@ -9,10 +9,10 @@ webmockr::stub_registry_clear()
 webmockr::stub_request("get", "https://dev.to/api/users/me") %>%
   webmockr::to_return(body = "success!", status = 200)
 
-response <- dev.to.ol::get_my_user()
+my_user <- dev.to.ol::get_my_user()
 
 test_that("stubbed response is 200", {
-  expect_is(response, "response")
-  expect_equal(response$status_code, 200)
+  expect_is(my_user, "response")
+  expect_equal(my_user$status_code, 200)
 })
 
--- tests/testthat/test-post_new_article.R
+++ tests/testthat/test-post_new_article.R
@@ -4,8 +4,9 @@ library(httr)
 
 test_that("post new article", {
   vcr::use_cassette("post_new_article", {
-    aa <- dev.to.ol::post_new_article(file = "./test.Rmd")
+    new_article <- dev.to.ol::post_new_article(file = "./test.Rmd")
   })
 
-  expect_is(aa, "response")
+  expect_is(new_article, "response")
+  expect_equal(new_article$status_code, 201)
 })
