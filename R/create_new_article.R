5907fa9f41e7b01a80209255506689f7e2fc6741
David Parr <davidrgparr@gmail.com>
2020-06-17 14:10:39 +0100

    start function to create new articles locally

---
+++ R/create_new_article.R
@@ -0,0 +1,32 @@
+#' @title Create a new article locally
+#' @description Make a new file with boilerplate YAML for posting to dev.to
+#' @param title string of the title
+#' @param series string of the series, Default: 'series'
+#' @param tags string of the YAML array of tags, Default: '['tag1', 'tag2']'
+#' @param file path of the file to create, should be a "*.Rmd", Default: ''
+#' @return By default the yaml boilerplate to the command line. If a file path is included it will create that file.
+#' @details DETAILS
+#' @examples
+#' \dontrun{
+#' if(interactive()){
+#'  create_new_article(title = "my title")
+#'  }
+#' }
+#' @seealso
+#'  \code{\link[glue]{glue}}
+#' @rdname create_new_article
+#' @export
+#' @importFrom glue glue
+
+create_new_article <-
+  function(title,
+           series = 'series',
+           tags = '["tag1", "tag2"]',
+           file = "") {
+    boilerplate_frontmatter <-
+      glue::glue(
+        '---\ntitle: "{title}"\noutput: github_document\nseries: "{series}"\ntags: {tags}\n---'
+      )
+
+    cat(boilerplate_frontmatter, file = file)
+  }
