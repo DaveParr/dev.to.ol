8672c9f70de736f4536f5d4f080242b8104524b4
David Parr <davidrgparr@gmail.com>
2020-06-25 16:32:05 +0100

    use description in new articles

--- R/create_new_article.R
+++ R/create_new_article.R
@@ -1,10 +1,11 @@
 #' @title Create a new article locally
 #' @description Make a new file with boilerplate YAML for posting to dev.to
 #' @param title string of the title
-#' @param series string of the series, Default: 'series'
+#' @param description string describing the article, Default: 'a description'
+#' @param series string of the series, Default: 'a series'
 #' @param tags string of the YAML array of tags, Default: '['tag1', 'tag2']'
 #' @param file path of the file to create, should be a "*.Rmd", Default: ''
-#' @return By default the yaml boilerplate to the command line. If a file path is included it will create that file.
+#' @return By default the yaml boilerplate to the command line. If a file path is included it will create that file. Descriptions do not currently appear to be used in the UI, but are used by integrations, like stackbit.
 #' @details DETAILS
 #' @examples
 #' \dontrun{
@@ -20,12 +21,13 @@
 
 create_new_article <-
   function(title,
-           series = 'series',
+           description = "a description",
+           series = 'a series',
            tags = '["tag1", "tag2"]',
            file = "") {
     boilerplate_frontmatter <-
       glue::glue(
-        '---\ntitle: "{title}"\noutput: github_document\nseries: "{series}"\ntags: {tags}\n---'
+        '---\ntitle: "{title}"\ndescription: {description}\nseries: "{series}"\ntags: {tags}\noutput: github_document\n---'
       )
 
     cat(boilerplate_frontmatter, file = file)
