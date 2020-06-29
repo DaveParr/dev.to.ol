99e25dfdfc819ac329ada0cb4122ec6b2e9ce201
Dave Parr <davidrgparr@gmail.com>
2020-06-29 16:35:39 +0100

    refresh new article response and test article update

--- tests/testthat/test-update_article.R
+++ tests/testthat/test-update_article.R
@@ -2,9 +2,15 @@ test_that("update an existing article", {
   # To record this test, you need have an article existing already, and to know the id of that article
 
   vcr::use_cassette("put_update_article", {
-    new_article <- dev.to.ol::update_article(id = "374133", file = "../fixtures/test_update.Rmd")
+    updated_article <- dev.to.ol::update_article(id = "374200", file = "../fixtures/test_update.Rmd")
   })
 
-  expect_is(new_article, "response")
-  expect_equal(new_article$status_code, 200)
+  expect_is(updated_article, "response")
+  expect_equal(updated_article$status_code, 200)
+
+  updated_article_content <- content(updated_article)
+
+  expect_equal(updated_article_content$title, "test title updated")
+  expect_equal(updated_article_content$description, "abstract text updated")
+  expect_equal(updated_article_content$tag_list, "test, test1, test2, updated1")
 })
