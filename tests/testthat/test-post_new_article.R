74fadf8c8353dde19f52b95aebbe51b7fee7c952
Dave Parr <davidrgparr@gmail.com>
2020-06-29 16:17:35 +0100

    test post content

--- tests/testthat/test-post_new_article.R
+++ tests/testthat/test-post_new_article.R
@@ -1,8 +1,14 @@
 test_that("post new article", {
   vcr::use_cassette("post_new_article", {
-    new_article <- dev.to.ol::post_new_article(file = "../fixtures/test.Rmd")
+    new_article <- dev.to.ol::post_new_article(file = "../fixtures/test_post.Rmd")
   })
 
   expect_is(new_article, "response")
   expect_equal(new_article$status_code, 201)
-})
+
+  new_article_content <- content(new_article)
+
+  expect_equal(new_article_content$title, "test title")
+  expect_equal(new_article_content$description, "abstract text")
+  expect_equal(new_article_content$tag_list, "test, test1, test2")
+  })
